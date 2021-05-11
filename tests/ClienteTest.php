fed71e8c32c687ce23172e5c1d91909488c17b27
Asuareztamayo <asuareztamayo@hotmail.com>
2021-05-11 14:09:19 +0200

    test

---
+++ tests/ClienteTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+use  App\Cliente;
+use  PHPUnit\Framework\TestCase;
+
+
+class ClienteTest extends TestCase 
+{
+    public function testClienteTieneIDCuandoSeCrea(){
+        $clienteUno = new Cliente();
+        $result = $clienteUno->identificacion;
+        $this->assertEquals(1, $result);
+    }
+}
+
+
