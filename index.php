ab8cd1e90852a3975025caca75b087e796fd3d86
Asuareztamayo <asuareztamayo@hotmail.com>
2021-05-20 12:32:04 +0200

    conexion heroku 2

--- index.php
+++ index.php
@@ -4,3 +4,6 @@ require_once("./vendor/autoload.php");
 require_once("./src/index.php");
 
 ?>
+
+
+
--- src/Database.php
+++ src/Database.php
@@ -22,10 +22,10 @@ class Database
     private function getConnection()
     {
 
-        $host = "localhost";
-        $user = "root";
-        $pass = "";
-        $database = "consultoria";
+        $host = "eu-cdbr-west-01.cleardb.com";
+        $user = "b255e1a921e094";
+        $pass = "ec91def9";
+        $database = "heroku_8d63085f3c0c37f";
         $charset = "utf-8";
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
         $pdo = new pdo("mysql:host={$host};dbname={$database};charset{$charset}", $user, $pass, $options);
@@ -34,3 +34,4 @@ class Database
         return $pdo;
     }
 }
+
